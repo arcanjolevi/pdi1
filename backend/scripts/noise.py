2775e09bc605af5912fb6d4eaa94e068db575f7a
Leví Cícero Arcanjo <48477760+arcanjolevi@users.noreply.github.com>
2022-01-28 10:33:11 -0300

    Update noise.py

--- backend/scripts/noise.py
+++ backend/scripts/noise.py
@@ -9,7 +9,6 @@ noise_name = ''
 
 if(noise == 0):
     noise_name = 's&p'
-    data = random_noise(img, mode=noise_name, seed=None, clip=True, amount=value)
 elif(noise == 1):
     noise_name = 'gaussian'
 elif(noise == 2):
@@ -24,6 +23,8 @@ value = float(sys.argv[4])
 img = io.imread(sys.argv[1])
 if(noise != 0):
     data = random_noise(img, mode=noise_name, seed=None, clip=True)
+else:
+    data = random_noise(img, mode=noise_name, seed=None, clip=True, amount=value)
 # io.imsave('noise_s&p.png', data)
 # data = random_noise(img, mode='gaussian', seed=None, clip=True)
 # io.imsave('noise_gauss.png', data)
